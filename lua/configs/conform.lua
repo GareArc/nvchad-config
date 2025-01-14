4ec39826d58bc71a901b63e9d3e4d48ffd61cdf2
GareArc <chen4851@purdue.edu>
2025-01-13 21:20:16 -0500

    feat: add osc 52 clipboard support

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,16 +1,14 @@
 local options = {
   formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-    py = { "autopep8" },
-    go = { "gofmt" },
+    python = { "isort", "autopep8" },
+    go = { "gofmt", "goimports" },
+    markdown = { "prettier" },
+    ["_"] = { "prettier" },
   },
-
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
+    -- timeout_ms = 500,
+    lsp_format = "fallback",
   },
 }
 
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,18 +1,8 @@
 return {
   {
-    'stevearc/conform.nvim',
-    opts = {
-      formatters_by_ft = {
-        python = { "isort", "autopep8" },
-        go = { "gofmt", "goimports" },
-        markdown = { "prettier" },
-        ["_"] = { "prettier" },
-      },
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        -- timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    },
-  }
+    "stevearc/conform.nvim",
+    opts = function()
+      return require "configs.conform"
+    end,
+  },
 }
