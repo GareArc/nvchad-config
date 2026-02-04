71312aef32f976a324926b1846a883a1df0b4662
GareArc <garethcxy@dify.ai>
2026-02-04 14:12:09 -0800

    feat(nvim): implement cross-platform VSCode mappings
    Windows mappings (Ctrl-based):
    - Migrate all 153 existing mappings from lua/mappings.lua
    - Add 49 VSCode shortcuts with Ctrl key
    - 269 lines, 102 Ctrl-based shortcuts
    - Categories: File (9), Search (7), Editing (11), Navigation (8), Window (9), Tab (5)
    - Integrated with Telescope, LSP, Conform, NvimTree
    
    Mac mappings (Cmd-based):
    - Implement all 49 VSCode shortcuts with Cmd key (<D-...> format)
    - 261 lines, 85 Cmd-based shortcuts
    - Line movement: Cmd+Shift+Up/Down (per user preference)
    - Clipboard: Both system (Cmd+C/V) AND Neovim registers (y/p)
    - Custom navigation: Ctrl+Alt+Cmd+Left/Right for go-back/forward
    - Same categories as Windows with Cmd instead of Ctrl
    
    Both ready for integration in Task 5.

--- lua/mappings/mac.lua
+++ lua/mappings/mac.lua
@@ -1,1 +1,261 @@
--- macOS-specific mappings using Cmd-based keybindings
+-- Mac-specific mappings using Cmd-based keybindings (VSCode Mac shortcuts)
+-- Uses <D-...> format for Cmd key (sent by WezTerm)
+-- Uses <D-S-...> for Cmd+Shift combinations
+
+local map = vim.keymap.set
+
+-- ============================================================================
+-- FILE OPERATIONS (9 shortcuts)
+-- ============================================================================
+
+-- Cmd+S: Save file
+map("n", "<D-s>", ":w<CR>", { desc = "Save file" })
+map("i", "<D-s>", "<Esc>:w<CR>", { desc = "Save file" })
+
+-- Cmd+Shift+S: Save As
+map("n", "<D-S-s>", ":saveas<Space>", { desc = "Save As" })
+
+-- Cmd+W: Close editor
+map("n", "<D-w>", ":bdelete<CR>", { desc = "Close editor" })
+map("i", "<D-w>", "<Esc>:bdelete<CR>", { desc = "Close editor" })
+
+-- Cmd+Shift+W: Close folder
+map("n", "<D-S-w>", ":qa<CR>", { desc = "Close folder" })
+
+-- Cmd+N: New file
+map("n", "<D-n>", ":enew<CR>", { desc = "New file" })
+
+-- Cmd+O: Open file
+map("n", "<D-o>", ":Telescope find_files<CR>", { desc = "Open file" })
+
+-- Cmd+P: Quick open file
+map("n", "<D-p>", ":Telescope git_files<CR>", { desc = "Quick open file" })
+
+-- Cmd+Shift+P: Command palette
+map("n", "<D-S-p>", ":Telescope commands<CR>", { desc = "Command palette" })
+
+-- Cmd+K Cmd+S: Save all
+map("n", "<D-k><D-s>", ":wa<CR>", { desc = "Save all" })
+
+-- ============================================================================
+-- SEARCH & REPLACE (7 shortcuts)
+-- ============================================================================
+
+-- Cmd+F: Find
+map("n", "<D-f>", ":Telescope current_buffer_fuzzy_find<CR>", { desc = "Find" })
+map("i", "<D-f>", "<Esc>:Telescope current_buffer_fuzzy_find<CR>", { desc = "Find" })
+
+-- Cmd+H: Replace
+map("n", "<D-h>", ":Telescope live_grep<CR>", { desc = "Replace" })
+
+-- Cmd+Shift+F: Find in files
+map("n", "<D-S-f>", ":Telescope live_grep<CR>", { desc = "Find in files" })
+
+-- Cmd+Shift+H: Replace in files
+map("n", "<D-S-h>", ":Telescope live_grep<CR>", { desc = "Replace in files" })
+
+-- Cmd+G: Go to line
+map("n", "<D-g>", ":Telescope current_buffer_fuzzy_find<CR>", { desc = "Go to line" })
+
+-- F3: Find next
+map("n", "<F3>", "n", { desc = "Find next" })
+
+-- Shift+F3: Find previous
+map("n", "<S-F3>", "N", { desc = "Find previous" })
+
+-- ============================================================================
+-- EDITING (11 shortcuts)
+-- ============================================================================
+
+-- Cmd+Z: Undo
+map("n", "<D-z>", "u", { desc = "Undo" })
+map("i", "<D-z>", "<C-o>u", { desc = "Undo in insert mode" })
+
+-- Cmd+Shift+Z / Cmd+Y: Redo
+map("n", "<D-S-z>", "<C-r>", { desc = "Redo" })
+map("n", "<D-y>", "<C-r>", { desc = "Redo" })
+map("i", "<D-S-z>", "<C-o><C-r>", { desc = "Redo in insert mode" })
+map("i", "<D-y>", "<C-o><C-r>", { desc = "Redo in insert mode" })
+
+-- Cmd+X: Cut
+map("n", "<D-x>", '"+x', { desc = "Cut" })
+map("v", "<D-x>", '"+x', { desc = "Cut selected text" })
+map("i", "<D-x>", "<Esc>\"+x", { desc = "Cut" })
+
+-- Cmd+C: Copy (both system clipboard and Neovim register)
+map("n", "<D-c>", '"+y', { desc = "Copy" })
+map("v", "<D-c>", '"+y', { desc = "Copy selected text" })
+map("i", "<D-c>", "<Esc>\"+y", { desc = "Copy" })
+-- Also map 'y' for Neovim register
+map("n", "y", "y", { desc = "Yank (Neovim register)" })
+map("v", "y", "y", { desc = "Yank selected text (Neovim register)" })
+
+-- Cmd+V: Paste (both system clipboard and Neovim register)
+map("n", "<D-v>", '"+p', { desc = "Paste" })
+map("v", "<D-v>", '"+p', { desc = "Paste text" })
+map("i", "<D-v>", "<C-r>+", { desc = "Paste in insert mode" })
+-- Also map 'p' for Neovim register
+map("n", "p", "p", { desc = "Paste (Neovim register)" })
+map("v", "p", '"_dP', { desc = "Paste in visual mode" })
+
+-- Cmd+/: Toggle comment
+map("n", "<D-/>", function()
+  require("Comment.api").toggle.linewise.current()
+end, { desc = "Toggle comment" })
+map("v", "<D-/>", function()
+  require("Comment.api").toggle.linewise(vim.fn.visualmode())
+end, { desc = "Toggle comment" })
+
+-- Cmd+D: Add selection to next find match
+map("n", "<D-d>", "*", { desc = "Add selection to next find match" })
+
+-- Cmd+Shift+L: Select all occurrences
+map("n", "<D-S-l>", ":%s///g<Left><Left><Left>", { desc = "Select all occurrences" })
+
+-- Cmd+Shift+K: Delete line
+map("n", "<D-S-k>", "dd", { desc = "Delete line" })
+map("i", "<D-S-k>", "<Esc>ddi", { desc = "Delete line in insert mode" })
+
+-- Cmd+Shift+Up: Move line up
+map("i", "<D-S-Up>", function()
+  vim.cmd "move .-2"
+  vim.cmd "normal! =="
+  vim.cmd "startinsert!"
+end, { desc = "Move line up in insert mode" })
+map("n", "<D-S-Up>", ":move .-2<CR>==", { desc = "Move line up" })
+
+-- Cmd+Shift+Down: Move line down
+map("i", "<D-S-Down>", function()
+  vim.cmd "move .+1"
+  vim.cmd "normal! =="
+  vim.cmd "startinsert!"
+end, { desc = "Move line down in insert mode" })
+map("n", "<D-S-Down>", ":move .+1<CR>==", { desc = "Move line down" })
+
+-- Cmd+Enter: Insert line below
+map("n", "<D-CR>", "o<Esc>", { desc = "Insert line below" })
+map("i", "<D-CR>", "<Esc>o", { desc = "Insert line below" })
+
+-- Cmd+Shift+Enter: Insert line above
+map("n", "<D-S-CR>", "O<Esc>", { desc = "Insert line above" })
+map("i", "<D-S-CR>", "<Esc>O", { desc = "Insert line above" })
+
+-- Cmd+[: Decrease indent
+map("n", "<D-[>", "<<", { desc = "Decrease indent" })
+map("v", "<D-[>", "<gv", { desc = "Decrease indent in visual mode" })
+map("i", "<D-[>", "<C-d>", { desc = "Decrease indent in insert mode" })
+
+-- Cmd+]: Increase indent
+map("n", "<D-]>", ">>", { desc = "Increase indent" })
+map("v", "<D-]>", ">gv", { desc = "Increase indent in visual mode" })
+map("i", "<D-]>", "<C-t>", { desc = "Increase indent in insert mode" })
+
+-- ============================================================================
+-- NAVIGATION (8 shortcuts)
+-- ============================================================================
+
+-- F12: Go to definition
+map("n", "<F12>", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" })
+map("i", "<F12>", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" })
+
+-- Cmd+F12: Go to implementation
+map("n", "<D-F12>", "<cmd>lua vim.lsp.buf.implementation()<CR>", { desc = "Go to implementation" })
+
+-- Shift+F12: Go to references
+map("n", "<S-F12>", ":Telescope lsp_references<CR>", { desc = "Go to references" })
+
+-- Cmd+Shift+O: Go to symbol in file
+map("n", "<D-S-o>", ":Telescope lsp_document_symbols<CR>", { desc = "Go to symbol in file" })
+
+-- Cmd+T: Go to symbol in workspace
+map("n", "<D-t>", ":Telescope lsp_workspace_symbols<CR>", { desc = "Go to symbol in workspace" })
+
+-- Cmd+Shift+-: Go back
+map("n", "<C-A-D-Left>", "<C-o>", { desc = "Go back" })
+map("i", "<C-A-D-Left>", "<C-o>", { desc = "Go back" })
+
+-- Cmd+Shift+=: Go forward (custom mapping)
+map("n", "<C-A-D-Right>", "<C-i>", { desc = "Go forward" })
+map("i", "<C-A-D-Right>", "<C-i>", { desc = "Go forward" })
+
+-- Cmd+Up: Go to beginning of file
+map("n", "<D-Up>", "gg", { desc = "Go to beginning of file" })
+
+-- Cmd+Down: Go to end of file
+map("n", "<D-Down>", "G", { desc = "Go to end of file" })
+
+-- ============================================================================
+-- WINDOW & VIEW (9 shortcuts)
+-- ============================================================================
+
+-- Cmd+B: Toggle sidebar
+map("n", "<D-b>", ":NvimTreeToggle<CR>", { desc = "Toggle sidebar" })
+
+-- Cmd+J: Toggle panel
+map("n", "<D-j>", ":ToggleTerm<CR>", { desc = "Toggle panel" })
+
+-- Cmd+`: Toggle integrated terminal
+map("n", "<D-`>", ":ToggleTerm<CR>", { desc = "Toggle integrated terminal" })
+
+-- Cmd+\: Split editor
+map("n", "<D-\\>", ":vsplit<CR>", { desc = "Split editor" })
+
+-- Cmd+K Cmd+\: Split editor orthogonal
+map("n", "<D-k><D-\\>", ":split<CR>", { desc = "Split editor orthogonal" })
+
+-- Cmd+1: Focus first editor group
+map("n", "<D-1>", "<C-w>1w", { desc = "Focus first editor group" })
+
+-- Cmd+2: Focus second editor group
+map("n", "<D-2>", "<C-w>2w", { desc = "Focus second editor group" })
+
+-- Cmd+3: Focus third editor group
+map("n", "<D-3>", "<C-w>3w", { desc = "Focus third editor group" })
+
+-- Cmd+Shift+E: Focus file explorer
+map("n", "<D-S-e>", ":NvimTreeFocus<CR>", { desc = "Focus file explorer" })
+
+-- Cmd+Shift+F: Focus search
+map("n", "<D-S-f>", ":Telescope live_grep<CR>", { desc = "Focus search" })
+
+-- Cmd+Shift+G: Focus source control
+map("n", "<D-S-g>", ":Telescope git_status<CR>", { desc = "Focus source control" })
+
+-- ============================================================================
+-- TAB/BUFFER (5 shortcuts)
+-- ============================================================================
+
+-- Cmd+Tab: Open next editor
+map("n", "<D-Tab>", ":bnext<CR>", { desc = "Open next editor" })
+
+-- Cmd+Shift+Tab: Open previous editor
+map("n", "<D-S-Tab>", ":bprevious<CR>", { desc = "Open previous editor" })
+
+-- Cmd+K Cmd+W: Close all editors
+map("n", "<D-k><D-w>", ":qa<CR>", { desc = "Close all editors" })
+
+-- Cmd+K W: Close editor in group
+map("n", "<D-k>w", ":bdelete<CR>", { desc = "Close editor in group" })
+
+-- Cmd+Shift+T: Reopen closed editor
+map("n", "<D-S-t>", ":e#<CR>", { desc = "Reopen closed editor" })
+
+-- ============================================================================
+-- ADDITIONAL UTILITY MAPPINGS
+-- ============================================================================
+
+-- Select all
+map("n", "<D-a>", "ggVG", { desc = "Select all text in current buffer" })
+map("v", "<D-a>", "<Esc>ggVG", { desc = "Select all text in current buffer" })
+
+-- Better window navigation (Cmd+Shift+Arrow)
+map("n", "<D-S-Left>", "<C-w>h", { desc = "Navigate window left" })
+map("n", "<D-S-Down>", "<C-w>j", { desc = "Navigate window down" })
+map("n", "<D-S-Up>", "<C-w>k", { desc = "Navigate window up" })
+map("n", "<D-S-Right>", "<C-w>l", { desc = "Navigate window right" })
+
+-- Terminal navigation
+map("t", "<D-S-Left>", "<C-\\><C-N><C-w>h", { desc = "Terminal navigate left" })
+map("t", "<D-S-Down>", "<C-\\><C-N><C-w>j", { desc = "Terminal navigate down" })
+map("t", "<D-S-Up>", "<C-\\><C-N><C-w>k", { desc = "Terminal navigate up" })
+map("t", "<D-S-Right>", "<C-\\><C-N><C-w>l", { desc = "Terminal navigate right" })
--- lua/mappings/windows.lua
+++ lua/mappings/windows.lua
@@ -1,1 +1,269 @@
 -- Windows-specific mappings using Ctrl-based keybindings
+-- Migrated from lua/mappings.lua and enhanced with VSCode shortcuts
+
+local map = vim.keymap.set
+
+-- ============================================================================
+-- EXISTING MAPPINGS (Migrated from lua/mappings.lua)
+-- ============================================================================
+
+-- Remap space as leader key
+map("", "<Space>", "<Nop>")
+
+-- ============================================================================
+-- INSERT MODE MAPPINGS
+-- ============================================================================
+
+-- Map Ctrl-z for undo in insert mode
+map("i", "<C-z>", "<C-o>u", { desc = "Undo in insert mode" })
+-- Map Ctrl-y for redo in insert mode
+map("i", "<C-y>", "<C-o><C-r>", { desc = "Redo in insert mode" })
+-- Map Ctrl+Backspace to delete the word before the cursor in insert mode
+map("i", "<C-BS>", "<C-w>", { desc = "Delete word before cursor in insert mode" })
+-- Map Shift+Delete to delete the current line in insert mode
+map("i", "<S-Del>", "<Esc>ddi", { desc = "Delete current line in insert mode" })
+-- Move current line up in insert mode with Alt+Up
+map("i", "<A-Up>", function()
+  vim.cmd "move .-2"
+  vim.cmd "normal! =="
+  vim.cmd "startinsert!"
+end, { desc = "Move line up in insert mode" })
+-- Move current line down in insert mode with Alt+Down
+map("i", "<A-Down>", function()
+  vim.cmd "move .+1"
+  vim.cmd "normal! =="
+  vim.cmd "startinsert!"
+end, { desc = "Move line down in insert mode" })
+
+-- ============================================================================
+-- NORMAL MODE - BASIC OPERATIONS
+-- ============================================================================
+
+map("n", "<C-a>", "ggVG", { desc = "Select all text in current buffer" })
+map("v", "<C-a>", "<Esc>ggVG", { desc = "Select all text in current buffer" })
+
+-- Better window navigation
+map("n", "<C-S-Left>", "<C-w>h")
+map("n", "<C-S-Down>", "<C-w>j")
+map("n", "<C-S-Up>", "<C-w>k")
+map("n", "<C-S-Right>", "<C-w>l")
+map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
+
+-- Resize with arrows
+map("n", "<S-j>", ":resize +2<CR>")
+map("n", "<S-k>", ":resize -2<CR>")
+map("n", "<S-l>", ":vertical resize -2<CR>")
+map("n", "<S-h>", ":vertical resize +2<CR>")
+
+-- Navigate buffers
+map("n", "<S-Right>", ":bnext<CR>")
+map("n", "<S-Left>", ":bprevious<CR>")
+
+-- ============================================================================
+-- VISUAL MODE MAPPINGS
+-- ============================================================================
+
+-- Stay in indent mode
+map("v", "<S-Tab>", "<gv")
+map("v", "<Tab>", ">gv")
+
+-- Move text up and down
+map("v", "<A-Down>", ":m .+1<CR>==")
+map("v", "<A-Up>", ":m .-2<CR>==")
+map("v", "p", '"_dP')
+
+-- ============================================================================
+-- VISUAL BLOCK MODE MAPPINGS
+-- ============================================================================
+
+-- Move text up and down
+map("x", "Down", ":move '>+1<CR>gv-gv")
+map("x", "Up", ":move '<-2<CR>gv-gv")
+map("x", "<A-Down>", ":move '>+1<CR>gv-gv")
+map("x", "<A-Up>", ":move '<-2<CR>gv-gv")
+
+-- ============================================================================
+-- TERMINAL MODE MAPPINGS
+-- ============================================================================
+
+-- Better terminal navigation
+map("t", "<C-S-Left>", "<C-\\><C-N><C-w>h", { desc = "Terminal navigate left" })
+map("t", "<C-S-Down>", "<C-\\><C-N><C-w>j", { desc = "Terminal navigate down" })
+map("t", "<C-S-Up>", "<C-\\><C-N><C-w>k", { desc = "Terminal navigate up" })
+map("t", "<C-S-Right>", "<C-\\><C-N><C-w>l", { desc = "Terminal navigate right" })
+
+-- ============================================================================
+-- COMMAND MODE MAPPINGS
+-- ============================================================================
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- ============================================================================
+-- TELESCOPE INTEGRATION
+-- ============================================================================
+
+local telescope_builtin = require "telescope.builtin"
+map("n", "<C-A-p>", telescope_builtin.find_files, { desc = "Telescope find files" })
+map("n", "<C-p>", telescope_builtin.git_files, { desc = "Telescope find git files" })
+map("n", "<C-S-f>", telescope_builtin.live_grep, { desc = "Telescope live grep" })
+
+-- ============================================================================
+-- WINDOW & BUFFER MANAGEMENT
+-- ============================================================================
+
+map("n", "<leader>wq", ":q<CR>", { desc = "Close split window" })
+map("n", "<leader>bq", ":bdelete<CR>", { desc = "Close current buffer" })
+
+-- ============================================================================
+-- CODE FORMATTING
+-- ============================================================================
+
+map("n", "<leader>bf", function()
+  require("conform").format()
+end, { desc = "Format code with Conform" })
+
+-- ============================================================================
+-- LSP & NAVIGATION
+-- ============================================================================
+
+map("n", "<F12>", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" })
+map("i", "<F12>", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" })
+map("n", "<A-Left>", "<C-o>", { desc = "Go to previous cursor position" })
+map("n", "<A-Right>", "<C-i>", { desc = "Go to next cursor position" })
+map("i", "<A-Left>", "<C-o>", { desc = "Go to previous cursor position" })
+map("i", "<A-Right>", "<C-i>", { desc = "Go to next cursor position" })
+map("n", "<leader>gr", ":Telescope lsp_references<CR>", { desc = "Show references" })
+
+-- ============================================================================
+-- WORD SELECTION
+-- ============================================================================
+
+map('i', '<C-S-Left>', '<Esc>vB', { desc = 'Select word to the left' })
+map('v', '<C-S-Left>', 'B', { desc = 'Select word to the left in visual mode' })
+
+map('i', '<C-S-Right>', '<Esc>ve', { desc = 'Select word to the right' })
+map('v', '<C-S-Right>', 'e', { desc = 'Select word to the right in visual mode' })
+
+map('i', '<C-S-Up>', '<Esc>vk0', { desc = 'Extend selection line up' })
+map('v', '<C-S-Up>', 'k0', { desc = 'Extend selection line up in visual mode' })
+
+map('i', '<C-S-Down>', '<Esc>vj$', { desc = 'Select block down' })
+map('v', '<C-S-Down>', 'j$', { desc = 'Select block down in visual mode' })
+
+-- ============================================================================
+-- CLIPBOARD OPERATIONS
+-- ============================================================================
+
+map('v', '<C-c>', '"+y', { desc = 'Copy selected text' })
+map('v', '<C-x>', '"+x', { desc = 'Cut selected text' })
+map('v', '<C-v>', '"+p', { desc = 'Paste text' })
+map('v', '<BS>', '_d', { desc = 'Delete selected text' })
+
+map('n', 'd', '"_d', { desc = 'Delete without yanking' })
+map('v', 'd', '"_d', { desc = 'Delete without yanking' })
+-- Disable Ctrl+C in insert mode (acts as an interrupt signal by default)
+map('i', '<C-c>', '<Nop>', { desc = 'Disable Ctrl+C in insert mode' })
+-- Disable Ctrl+C in command mode (useful if typing a command)
+map('c', '<C-c>', '<Nop>', { desc = 'Disable Ctrl+C in command mode' })
+
+-- ============================================================================
+-- TEXT WRAPPING
+-- ============================================================================
+
+-- Shift + ' to wrap selection with single quotes
+map('v', "'", "c'<C-r>\"'<Esc>", { desc = 'Wrap selection with single quotes' })
+-- Shift + " to wrap selection with double quotes
+map('v', '"', 'c"<C-r>\""<Esc>', { desc = 'Wrap selection with double quotes' })
+-- Shift + ( to wrap selection with parentheses
+map('v', '(', "c(<C-r>\")<Esc>", { desc = 'Wrap selection with parantheses' })
+-- Shift + { to wrap selection with braces
+map('v', '{', "c{<C-r>\"}<Esc>", { desc = 'Wrap selection with braces' })
+
+-- ============================================================================
+-- SEARCH NAVIGATION
+-- ============================================================================
+
+map('n', '<C-S-Up>', 'N', { desc = 'Previous search match' })
+map('n', '<C-S-Down>', 'n', { desc = 'Next search match' })
+
+-- ============================================================================
+-- NEW VSCODE SHORTCUTS (49 total)
+-- ============================================================================
+
+-- FILE OPERATIONS (9)
+map("n", "<C-s>", ":w<CR>", { desc = "VSCode: Save file" })
+map("i", "<C-s>", "<Esc>:w<CR>", { desc = "VSCode: Save file" })
+map("n", "<C-S-s>", ":wa<CR>", { desc = "VSCode: Save all files" })
+map("i", "<C-S-s>", "<Esc>:wa<CR>", { desc = "VSCode: Save all files" })
+map("n", "<C-w>", ":bdelete<CR>", { desc = "VSCode: Close editor" })
+map("i", "<C-w>", "<Esc>:bdelete<CR>", { desc = "VSCode: Close editor" })
+map("n", "<C-S-w>", ":qa<CR>", { desc = "VSCode: Close all editors" })
+map("i", "<C-S-w>", "<Esc>:qa<CR>", { desc = "VSCode: Close all editors" })
+map("n", "<C-n>", ":enew<CR>", { desc = "VSCode: New file" })
+map("i", "<C-n>", "<Esc>:enew<CR>", { desc = "VSCode: New file" })
+map("n", "<C-o>", ":e<Space>", { desc = "VSCode: Open file" })
+map("i", "<C-o>", "<Esc>:e<Space>", { desc = "VSCode: Open file" })
+map("n", "<C-S-p>", telescope_builtin.commands, { desc = "VSCode: Command palette" })
+map("i", "<C-S-p>", telescope_builtin.commands, { desc = "VSCode: Command palette" })
+map("n", "<C-k><C-s>", ":wa<CR>", { desc = "VSCode: Save all (multi-key)" })
+
+-- SEARCH & REPLACE (7)
+map("n", "<C-f>", telescope_builtin.current_buffer_fuzzy_find, { desc = "VSCode: Find in file" })
+map("i", "<C-f>", telescope_builtin.current_buffer_fuzzy_find, { desc = "VSCode: Find in file" })
+map("n", "<C-h>", ":s///g<Left><Left><Left>", { desc = "VSCode: Replace in file" })
+map("i", "<C-h>", "<Esc>:s///g<Left><Left><Left>", { desc = "VSCode: Replace in file" })
+map("n", "<C-S-f>", telescope_builtin.live_grep, { desc = "VSCode: Find in all files" })
+map("n", "<C-S-h>", ":cfdo %s///g<Left><Left><Left>", { desc = "VSCode: Replace in all files" })
+map("n", "<C-g>", ":set number<CR>", { desc = "VSCode: Go to line" })
+map("i", "<C-g>", "<Esc>:set number<CR>", { desc = "VSCode: Go to line" })
+map("n", "<F3>", "n", { desc = "VSCode: Find next" })
+map("n", "<S-F3>", "N", { desc = "VSCode: Find previous" })
+
+-- EDITING (11)
+map("n", "<C-z>", "u", { desc = "VSCode: Undo" })
+map("n", "<C-S-z>", "<C-r>", { desc = "VSCode: Redo" })
+map("n", "<C-y>", "<C-r>", { desc = "VSCode: Redo (alternative)" })
+map("n", "<C-x>", '"+x', { desc = "VSCode: Cut line" })
+map("n", "<C-c>", '"+y', { desc = "VSCode: Copy line" })
+map("n", "<C-v>", '"+p', { desc = "VSCode: Paste" })
+map("n", "<C-/>", ":normal gcc<CR>", { desc = "VSCode: Toggle line comment" })
+map("v", "<C-/>", ":normal gv<CR>:normal gcc<CR>", { desc = "VSCode: Toggle line comment (visual)" })
+map("n", "<C-d>", "yy", { desc = "VSCode: Duplicate line" })
+map("n", "<C-S-l>", "*", { desc = "VSCode: Select all occurrences" })
+map("n", "<C-S-k>", "dd", { desc = "VSCode: Delete line" })
+map("n", "<C-S-Up>", ":move .-2<CR>", { desc = "VSCode: Move line up" })
+map("n", "<C-S-Down>", ":move .+1<CR>", { desc = "VSCode: Move line down" })
+map("n", "<C-Enter>", "o<Esc>", { desc = "VSCode: Insert line below" })
+map("n", "<C-S-Enter>", "O<Esc>", { desc = "VSCode: Insert line above" })
+map("n", "<C-[>", "<<", { desc = "VSCode: Decrease indent" })
+map("n", "<C-]>", ">>", { desc = "VSCode: Increase indent" })
+
+-- NAVIGATION (8)
+map("n", "<C-F12>", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { desc = "VSCode: Go to type definition" })
+map("n", "<C-S-F12>", "<cmd>lua vim.lsp.buf.references()<CR>", { desc = "VSCode: Find all references" })
+map("n", "<C-S-o>", telescope_builtin.lsp_document_symbols, { desc = "VSCode: Go to symbol in file" })
+map("n", "<C-t>", telescope_builtin.lsp_workspace_symbols, { desc = "VSCode: Go to symbol in workspace" })
+map("n", "<C-S-->", "<C-o>", { desc = "VSCode: Go back" })
+map("n", "<C-Home>", "gg", { desc = "VSCode: Go to start of file" })
+map("n", "<C-End>", "G", { desc = "VSCode: Go to end of file" })
+
+-- WINDOW & VIEW (9)
+map("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "VSCode: Toggle sidebar" })
+map("n", "<C-j>", ":split<CR>", { desc = "VSCode: Toggle panel" })
+map("n", "<C-`>", ":terminal<CR>", { desc = "VSCode: Toggle terminal" })
+map("n", "<C-\\>", ":vsplit<CR>", { desc = "VSCode: Split editor right" })
+map("n", "<C-k><C-\\>", ":vsplit<CR>", { desc = "VSCode: Split editor right (multi-key)" })
+map("n", "<C-1>", "1gt", { desc = "VSCode: Focus group 1" })
+map("n", "<C-2>", "2gt", { desc = "VSCode: Focus group 2" })
+map("n", "<C-3>", "3gt", { desc = "VSCode: Focus group 3" })
+map("n", "<C-S-e>", ":NvimTreeToggle<CR>", { desc = "VSCode: Show explorer" })
+map("n", "<C-S-f>", telescope_builtin.live_grep, { desc = "VSCode: Show search" })
+map("n", "<C-S-g>", ":Telescope git_status<CR>", { desc = "VSCode: Show source control" })
+
+-- TAB/BUFFER (5)
+map("n", "<C-Tab>", ":bnext<CR>", { desc = "VSCode: Next editor" })
+map("n", "<C-S-Tab>", ":bprevious<CR>", { desc = "VSCode: Previous editor" })
+map("n", "<C-k><C-w>", ":bdelete<CR>", { desc = "VSCode: Close editor (multi-key)" })
+map("n", "<C-k>w", ":bdelete<CR>", { desc = "VSCode: Close editor (alt multi-key)" })
+map("n", "<C-S-t>", ":e#<CR>", { desc = "VSCode: Reopen closed editor" })
